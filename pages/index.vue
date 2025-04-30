68b75bd04e9aa5de9fdd7dc7ae99fec1ffd1ef5c
degospel <degospel22@gmail.com>
2025-04-30 15:18:19 +0100

    added text

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="py-6">
-            <h1 class="text-2xl">Trending</h1>
+            <h1 class="text-2xl">Trending Content</h1>
         </div>
 
         <div>
